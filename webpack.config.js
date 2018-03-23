fb6e58ba20781fc8065ed5a5697524fb4ac59e8e
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-03-23 12:11:25 -0700

    Add dotenv package and all requirements for it to work

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require ('html-webpack-plugin');
 const CleanWebpackPlugin = require ('clean-webpack-plugin');
 const UglifyJsPlugin = require ('uglifyjs-webpack-plugin');
+const Dotenv = require ('dotenv-webpack');
 
 module.exports = {
   entry: './src/js/main.js',
@@ -20,7 +21,8 @@ module.exports = {
       title: 'Better Doctor | Ernest Bruno',
       template: './src/html/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
